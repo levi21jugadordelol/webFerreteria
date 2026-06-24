90238691b7b5f47e15736861ead0f461c15c8a9a
saul rodriguez <editor.taisei@gmail.com>
2026-06-24 00:24:18 -0500

    chore(debug): verify cloudinary admin resources access

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,12 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import { env } from "./env.js";
 
-console.log("CLOUDINARY CHECK", {
-  cloudName: env.CLOUDINARY.CLOUD_NAME,
-  apiKeyExists: !!env.CLOUDINARY.API_KEY,
-  apiSecretExists: !!env.CLOUDINARY.API_SECRET,
-});
-
 cloudinary.config({
   cloud_name: env.CLOUDINARY.CLOUD_NAME,
   api_key: env.CLOUDINARY.API_KEY,
@@ -16,15 +10,28 @@ cloudinary.config({
 
 cloudinary.api
   .ping()
-  .then((res) => {
-    console.log("CLOUDINARY PING OK", res);
-  })
-  .catch((error) => {
+  .then((res) => console.log("CLOUDINARY PING OK", res))
+  .catch((error) =>
     console.error("CLOUDINARY PING ERROR", {
       message: error?.message,
       name: error?.name,
       http_code: error?.http_code,
-    });
-  });
+    }),
+  );
+
+cloudinary.api
+  .resources({ max_results: 1 })
+  .then((res) =>
+    console.log("CLOUDINARY RESOURCES OK", {
+      resources: res.resources?.length,
+    }),
+  )
+  .catch((error) =>
+    console.error("CLOUDINARY RESOURCES ERROR", {
+      message: error?.message,
+      name: error?.name,
+      http_code: error?.http_code,
+    }),
+  );
 
 export default cloudinary;
