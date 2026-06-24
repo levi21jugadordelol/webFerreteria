5f612e468d599b357b52b08f75cfaa26c18bf593
saul rodriguez <editor.taisei@gmail.com>
2026-06-24 02:08:44 -0500

    test(cloudinary): use cloudinary url configuration

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,10 +1,44 @@
+// import { v2 as cloudinary } from "cloudinary";
+// import { env } from "./env.js";
+
+// cloudinary.config({
+//   cloud_name: env.CLOUDINARY.CLOUD_NAME,
+//   api_key: env.CLOUDINARY.API_KEY,
+//   api_secret: env.CLOUDINARY.API_SECRET,
+//   secure: true,
+// });
+
+// cloudinary.api
+//   .ping()
+//   .then((res) => console.log("CLOUDINARY PING OK", res))
+//   .catch((error) =>
+//     console.error("CLOUDINARY PING ERROR", {
+//       message: error?.message,
+//       name: error?.name,
+//       http_code: error?.http_code,
+//     }),
+//   );
+
+// cloudinary.api
+//   .resources({ max_results: 1 })
+//   .then((res) =>
+//     console.log("CLOUDINARY RESOURCES OK", {
+//       resources: res.resources?.length,
+//     }),
+//   )
+//   .catch((error) =>
+//     console.error("CLOUDINARY RESOURCES ERROR", {
+//       message: error?.message,
+//       name: error?.name,
+//       http_code: error?.http_code,
+//     }),
+//   );
+
+// export default cloudinary;
+
 import { v2 as cloudinary } from "cloudinary";
-import { env } from "./env.js";
 
 cloudinary.config({
-  cloud_name: env.CLOUDINARY.CLOUD_NAME,
-  api_key: env.CLOUDINARY.API_KEY,
-  api_secret: env.CLOUDINARY.API_SECRET,
   secure: true,
 });
 
@@ -13,21 +47,6 @@ cloudinary.api
   .then((res) => console.log("CLOUDINARY PING OK", res))
   .catch((error) =>
     console.error("CLOUDINARY PING ERROR", {
-      message: error?.message,
-      name: error?.name,
-      http_code: error?.http_code,
-    }),
-  );
-
-cloudinary.api
-  .resources({ max_results: 1 })
-  .then((res) =>
-    console.log("CLOUDINARY RESOURCES OK", {
-      resources: res.resources?.length,
-    }),
-  )
-  .catch((error) =>
-    console.error("CLOUDINARY RESOURCES ERROR", {
       message: error?.message,
       name: error?.name,
       http_code: error?.http_code,
